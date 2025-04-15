00846258e90b7e3c852fe4a5766867f07bbef30b
김영서 <dudtj926@naver.com>
2025-04-15 13:40:28 +0900

    fix: mixed content proxy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    global: {},
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://ec2-52-79-228-10.ap-northeast-2.compute.amazonaws.com:8080',
+        changeOrigin: true,
+      },
+    },
   },
 });
