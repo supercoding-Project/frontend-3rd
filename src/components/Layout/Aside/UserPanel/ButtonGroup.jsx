3d80b6bfff0c787465cf9925c907ede7a5f05bba
junhui4005 <100547902+junhui4005@users.noreply.github.com>
2025-04-20 00:52:56 +0900

    [feature/dev] 알림 & 마이페이지 수정 로직 추가
    알림 소켓 연결부터 알림 불러오는 것까지 모두 추가했습니다.
    기존 서버에서 구현된 비밀번호 변경과 이미지 변경 기능과 관련하여 미흡한 부분이 있어 추가 했습니다

--- src/components/Layout/Aside/UserPanel/ButtonGroup.jsx
+++ src/components/Layout/Aside/UserPanel/ButtonGroup.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { BsGearFill, BsChatRightDotsFill, BsBellFill } from 'react-icons/bs';
 import styled from 'styled-components';
-import ButtonWithBadge from './ButtonWithBadge'; // 나중에 배지 컴포넌트 사용
+import ButtonWithBadge from './ButtonWithBadge';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+const baseUrl = 'http://ec2-52-79-228-10.ap-northeast-2.compute.amazonaws.com:8080';
 
 const openChatList = () => {
   window.open('/chat-list', '_blank', 'width=500, height=700, top=300, left=500, noopener,noreferrer');
@@ -18,18 +21,58 @@ const ButtonGroupContainer = styled.div`
 
 const ButtonGroup = () => {
   const navigate = useNavigate();
+  const [alarmCount, setAlarmCount] = useState(0);
+
+  const fetchAlarmCount = async () => {
+    try {
+      const token = localStorage.getItem('access_token');
+      if (!token) {
+        console.warn('No access token found.');
+        return;
+      }
+  
+      const response = await axios.get(`${baseUrl}/api/v1/alarms`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+  
+      console.log('알림 API 응답:', response.data);
+      setAlarmCount(response.data.data);
+    } catch (error) {
+      console.error('알림 개수 조회 실패:', error);
+    }
+  };
+  
+  
+
+  useEffect(() => {
+    fetchAlarmCount();
+  
+    const interval = setInterval(() => {
+      fetchAlarmCount();
+    }, 10000);
+  
+    const onAlarmUpdated = () => {
+      const count = localStorage.getItem("alarmCount");
+      setAlarmCount(parseInt(count) || 0);
+    };
+  
+    window.addEventListener("alarmCountUpdated", onAlarmUpdated);
+  
+    return () => {
+      clearInterval(interval);
+      window.removeEventListener("alarmCountUpdated", onAlarmUpdated);
+    };
+  }, []);
+  
+
   return (
     <ButtonGroupContainer>
       <ButtonWithBadge $icon={<BsGearFill />} onClick={() => navigate('/myPage')} />
-      <ButtonWithBadge
-        $icon={<BsChatRightDotsFill />}
-        $badgeCount={4}
-        onClick={() => {
-          openChatList();
-        }}
-      />
-      <Link to='/notifications'>
-        <ButtonWithBadge $icon={<BsBellFill />} $badgeCount={2} />
+      <ButtonWithBadge $icon={<BsChatRightDotsFill />} $badgeCount={4} onClick={openChatList} />
+      <Link to="/notifications">
+      <ButtonWithBadge $icon={<BsBellFill />} $badgeCount={alarmCount > 0 ? alarmCount : 0} />
       </Link>
     </ButtonGroupContainer>
   );
